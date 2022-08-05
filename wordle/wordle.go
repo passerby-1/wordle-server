dbf18851a124e81b3bf61f176d207efec3a4497b
passerby-1 <ps1@passerby1.dev>
2022-08-06 02:28:13 +0900

    memo

--- wordle/wordle.go
+++ wordle/wordle.go
@@ -19,17 +19,24 @@ func Wordle() {
 	selectedWord := SelectWord(wordList)
 	reader := bufio.NewReader(os.Stdin)
 
+	// DEBUG
+	fmt.Printf("SelectedWord:%v\n", selectedWord)
+
 	var guesses []map[string][WORD_LENGTH]string
 
 	var guessCount int
 	for guessCount = 0; guessCount < MAX_GUESSES; guessCount++ { // MAX_GUESSESの回数繰り返し
 		fmt.Printf("Enter your guess (%v/%v): ", guessCount+1, MAX_GUESSES)
-
 		guessWord, err := reader.ReadString('\n')
 		if err != nil {
 			log.Fatalln(err)
 		}
-		guessWord = strings.ToUpper(guessWord[:len(guessWord)-1])
+
+		guessWord = strings.ToUpper(guessWord[:len(guessWord)-2]) // 改行の消去と大文字化
+
+		// 元は\nのみを消すため1文字分削っていたが、Windows環境では、1文字削りでは動作しなかった
+		// 長さを調べたところ、selectedWordが5文字なのに対し、guessWordは6文字となっていた
+		// Windowsの\r\nとUnixの\nで場合分けする必要がある
 
 		if guessWord == selectedWord { // 正解なら
 			fmt.Println("You guessed right!")
@@ -50,7 +57,15 @@ func Wordle() {
 			// 昇順にソートされた文字列スライスwordleWordsと文字列guessWordを受け取り、このスライスの中にguessWordがあるかを二分探索する
 			// 存在する場合、その順番を返す (if文で引っかかっていないものだけなので、ここでは存在しない場合のみとなる)
 			// 存在しない場合、昇順の文字列スライス中何番目にguessWordを挿入すれば良いかのインデックスを返す
-			if i < len(wordList) && wordList[i] == guessWord {
+			// リスト中に単語が存在しなければ、条件に当てはまらず使用してはいけない単語判定 (elseへ)
+
+			// 現状検索が上手くいっていない (恐らく、上記\r\n, \n問題?)
+
+			// DEBUG
+			// fmt.Printf("i:%v, len(wordList):%v, wordList[i]:%v, wordList[i-1]:%v, wordList[i+1]:%v\n", i, len(wordList), wordList[i], wordList[i-1], wordList[i+1])
+			// fmt.Printf("i<len(wordList):%v, wordList[i-1] == guessWord:%v\n", i < len(wordList), wordList[i-1] == guessWord)
+
+			if (i < len(wordList)) && (wordList[i-1] == guessWord) {
 				colorVector := GetFilledColorVector("Grey")
 
 				// stores whether an index is allowed to cause another index to be yellow
